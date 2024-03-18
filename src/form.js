f808364931f91cfff2e64f615dd98720fe4fbd33
maelanglois <150166864+maelanglois@users.noreply.github.com>
2024-03-18 14:20:25 +0100

    corrections

--- src/form.js
+++ src/form.js
@@ -1,17 +1,21 @@
 const Form = class{
+    constructor() {
+        el = document.querySelector('.root');
+        run();
+    };
 
     render() {
         return`
         <form method="post">
             <div class="num-ademe">
-                <input type="text" class="" placeholder="Numéro Ademe" name="num-ademe">
+                <input type="text" class="" placeholder="Numéro Ademe" name="num-ademe" class="form">
             </div>
         </form>`
-    }
+    };
 
     run() {
-        document.querySelector('.root').innerHTML = render();
-    }
-}
+        el.innerHTML = render();
+    };
+};
 
 export default Form;
