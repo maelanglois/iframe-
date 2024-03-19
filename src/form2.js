53504f6168a66f844e10725ce7cd8a8d9ee68b20
maelanglois <150166864+maelanglois@users.noreply.github.com>
2024-03-19 13:54:57 +0100

    css form 2

--- src/form2.js
+++ src/form2.js
@@ -30,63 +30,82 @@ const UserForm = class {
     <div class="user-form">
       <div class="user-questions">
         <form>
-          <div class="user-info">
-            <div class="user-line"
+          <div class="reg-disp">
+            <div class="reg-inp">
               <label for="firstname">Prénom</label>
               <input name="firstname" id="firstname" type="text" placeholder="Votre prénom" required>
-
+            </div>
+            <div class="reg-inp">
               <label for="lastname">Nom</label>
               <input name="lastname" id="lastname" type="text" placeholder="Votre nom" required>
             </div>
-
-            <div class="user-line">
+            <div class="reg-inp">
               <label for="phone">N° de téléphone</label>
               <input name="phone" id="phone" type="tel" placeholder="Votre numéro de téléphone" required>
-
+            </div>
+            <div class="reg-inp">
               <label for="email">Adresse Mail</label>
               <input name="email" id="email" type="email" placeholder="Votre adresse email" required>
             </div>
-
-            <div class="user-line"
+            <div class="reg-inp">
               <label for="address">Adresse de votre bien</label>
               <input name="address" id="address" type="text" placeholder="Votre adresse postale" required>
             </div>
           </div>
+          <hr>
           <div>
-            <div>
+            <div calss="reg-disp">
               <p>Je suis...</p>
-              <input type="radio" id="landlord_in" name="user_type" value="landlord_in" required>
-              <label for="landlord_in">Propriétaire occupant</label>
-              <input type="radio" id="landlord_out" name="user_type" value="landlord_out" required>
-              <label for="landlord_out">Propriétaire bailleur</label>
-              <input type="radio" id="landlord_futur" name="user_type" value="landlord_futur" required>
-              <label for="landlord_futur">Futur Propriétaire</label>
-              <input type="radio" id="tenant" name="user_type" value="tenant" required>
-              <label for="tenant">Locataire</label>
+              <div class="reg-info">
+                <input type="radio" id="landlord_in" name="user_type" value="landlord_in" required>
+                <label for="landlord_in">Propriétaire occupant</label>
+              </div>
+              <div class="reg-info">
+                <input type="radio" id="landlord_out" name="user_type" value="landlord_out" required>
+                <label for="landlord_out">Propriétaire bailleur</label>
+              </div>
+              <div class="reg-info">
+                <input type="radio" id="landlord_futur" name="user_type" value="landlord_futur" required>
+                <label for="landlord_futur">Futur Propriétaire</label>
+              </div>
+              <div class="reg-info">
+                <input type="radio" id="tenant" name="user_type" value="tenant" required>
+                <label for="tenant">Locataire</label>
+              </div>
             </div>
-            <div>
-              <p>Comptez-vous réaliser des travaux de rénovations prochainement ?</p>
-              <input list="renovation" required>
-              <datalist id="renovation">
-              <option value="Oui, dans moins de 3 mois">
-              <option value="Oui, dans les 6 mois">
-              <option value="Oui, dans plus de 6 mois">
-              <option value="Non">
-              <option value="Je ne sais pas">
-              </datalist>
+            <hr>
+            <div class="reg-disp-col">
+              <div>Comptez-vous réaliser des travaux de rénovations prochainement ?</div>
+                <select id="renovation" class="reg-select">
+                  <option value="Oui, dans moins de 3 mois">Oui, dans moins de 3 mois</option>
+                  <option value="Oui, dans les 6 mois">Oui, dans les 6 mois</option>
+                  <option value="Oui, dans plus de 6 mois">Oui, dans plus de 6 mois</option>
+                  <option value="Non">Non</option>
+                  <option value="Je ne sais pas">Je ne sais pas</option>
+                </select>
             </div>
-            <div>
-              <p>Avez-vous déjà un artisan ?</p>
-              <input type="radio" id="craftsman_yes" name="craftsman" value="craftsman_yes" required>
-              <label for="craftsman_yes">Oui</label>
-              <input type="radio" id="craftmans_no" name="craftsman" value="craftmans_no" required>
-              <label for="craftsman_no">Non</label>
+            <hr>
+            <div class="reg-disp-col">
+              <div>Avez-vous déjà un artisan ?</div>
+              <div class="reg-abo">
+                <input type="radio" id="craftsman_yes" name="craftsman" value="craftsman_yes" required>
+                <label for="craftsman_yes">Oui</label>
+              </div>
+              <div class="reg-abo">
+                <input type="radio" id="craftmans_no" name="craftsman" value="craftmans_no" required>
+                <label for="craftsman_no">Non</label>
+              </div>
             </div>
-            <div>
-              <input type="checkbox" id="" name="" value="">
-              <label for="">J'accepte d'être recontacté par un artisan partenaire pour un devis estimatif</label><br>
-              <input type="checkbox" id="vehicle2" name="vehicle2" value="Car">
-              <label for="">Je m'abonne à la newsletter Optimo</label><br>
+            <hr>
+            <div class="reg-disp-col">
+              <div class="reg-abo">
+                <input type="checkbox" id="" name="" value="">
+                <label for="">J'accepte d'être recontacté par un artisan partenaire pour un devis estimatif</label>
+              </div>
+              <div class="reg-abo">
+                <input type="checkbox" id="vehicle2" name="vehicle2" value="Car">
+                <label for="">Je m'abonne à la newsletter Optimo</label>
+              </div>
             </div>
           </div>
           <input type="submit" class="submit" value="Obtenir mes scénari de travaux ->">
