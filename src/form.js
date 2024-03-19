ef9a7e8394c84dc9486f13bb8ac855019a316989
sarahlte <sarah_leconte@msn.com>
2024-03-19 16:10:39 +0100

    les dernieres modifs

--- src/form.js
+++ src/form.js
@@ -1,7 +1,14 @@
 const Form = class {
   constructor() {
     this.el = document.querySelector('.root');
-
+    fetch('https://dpe-api-service-dev-xfyprtzkyq-ew.a.run.app/token', {
+      method: 'POST',
+      headers: {
+        'accept': 'application/json',
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: 'grant_type=&username=coda_school_student&password=XEUgknQ)GDCf%40K%3A%5D2T6%60c_Y%3Ddb%26%2BM9&scope=&client_id=&client_secret='
+    }).then(response=>console.log(response));
     this.run();
   };
 
