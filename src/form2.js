e886c0aa2774e7299b914d40dc3b6a2abc98d8d6
TaliorTheRogue <donribeiro45@gmail.com>
2024-03-19 10:06:44 +0100

    form2 required input

--- src/form2.js
+++ src/form2.js
@@ -31,35 +31,35 @@ const UserForm = class {
       <form>
         <div>
           <label for="firstname">Prénom</label>
-          <input name="firstname" id="firstname" type="text" placeholder="Votre prénom">
+          <input name="firstname" id="firstname" type="text" placeholder="Votre prénom" required>
 
           <label for="lastname">Nom</label>
-          <input name="lastname" id="lastname" type="text" placeholder="Votre nom">
+          <input name="lastname" id="lastname" type="text" placeholder="Votre nom" required>
 
           <label for="phone">N° de téléphone</label>
-          <input name="phone" id="phone" type="tel" placeholder="Votre numéro de téléphone">
+          <input name="phone" id="phone" type="tel" placeholder="Votre numéro de téléphone" required>
 
           <label for="email">Adresse Mail</label>
-          <input name="email" id="email" type="email" placeholder="Votre adresse email">
+          <input name="email" id="email" type="email" placeholder="Votre adresse email" required>
 
           <label for="address">Adresse de votre bien</label>
-          <input name="address" id="address" type="text" placeholder="Votre adresse email">
+          <input name="address" id="address" type="text" placeholder="Votre adresse postale" required>
         </div>
         <div>
           <div>
             <p>Je suis...</p>
-            <input type="radio" id="landlord_in" name="user_type" value="landlord_in">
+            <input type="radio" id="landlord_in" name="user_type" value="landlord_in" required>
             <label for="landlord_in">Propriétaire occupant</label>
-            <input type="radio" id="landlord_out" name="user_type" value="landlord_out">
+            <input type="radio" id="landlord_out" name="user_type" value="landlord_out" required>
             <label for="landlord_out">Propriétaire bailleur</label>
-            <input type="radio" id="landlord_futur" name="user_type" value="landlord_futur">
+            <input type="radio" id="landlord_futur" name="user_type" value="landlord_futur" required>
             <label for="landlord_futur">Futur Propriétaire</label>
-            <input type="radio" id="tenant" name="user_type" value="tenant">
+            <input type="radio" id="tenant" name="user_type" value="tenant" required>
             <label for="tenant">Locataire</label>
           </div>
           <div>
             <p>Comptez-vous réaliser des travaux de rénovations prochainement ?</p>
-            <input list="renovation">
+            <input list="renovation" required>
             <datalist id="renovation">
             <option value="Oui, dans moins de 3 mois">
             <option value="Oui, dans les 6 mois">
@@ -70,19 +70,19 @@ const UserForm = class {
           </div>
           <div>
             <p>Avez-vous déjà un artisan ?</p>
-            <input type="radio" id="craftsman_yes" name="craftsman" value="craftsman_yes">
+            <input type="radio" id="craftsman_yes" name="craftsman" value="craftsman_yes" required>
             <label for="craftsman_yes">Oui</label>
-            <input type="radio" id="craftmans_no" name="craftsman" value="craftmans_no">
+            <input type="radio" id="craftmans_no" name="craftsman" value="craftmans_no" required>
             <label for="craftsman_no">Non</label>
           </div>
           <div>
             <input type="checkbox" id="" name="" value="">
             <label for="">J'accepte d'être recontacté par un artisan partenaire pour un devis estimatif</label><br>
             <input type="checkbox" id="vehicle2" name="vehicle2" value="Car">
-            <label for="vehicle2">Je m'abonne à la newsletter Optimo</label><br>
+            <label for="">Je m'abonne à la newsletter Optimo</label><br>
           </div>
         </div>
-        <input type="submit" value="Obtenir mes scénari de travaux ->">
+        <input type="submit" class="submit" value="Obtenir mes scénari de travaux ->">
       </form> 
     </div>
     <div>
